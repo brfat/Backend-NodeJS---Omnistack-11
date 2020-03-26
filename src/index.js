4ece6f88e8291b9ff8416fa30c163b171a05742d
Bruno jose da Silva <brunofat@Mac-mini-de-Mac.local>
2020-03-26 19:16:18 -0300

    removing comments

--- src/index.js
+++ src/index.js
@@ -17,31 +17,3 @@ app.use(routes);
 
 
 app.listen(3333);
-
-/*
-
-Métodos HTTP:
-
-GET: Buscar/listar uma informação no back-end
-POST: Criar uma informação no back-end
-PUT: Alterar uma informação no back-end
-DELETE: Deletar uma informação no back-end
-
-*/
-
-/*
-
-Tipos de parametros:
-
-Query Params: Parametros nomeados enviados na rota após "?" (Filtros, paginação)
-Route Params: Parâmetros utilizados para identificar recursos
-Request Body: Corpo da requisição, utilizado para criar ou alterar recursos 
-
-*/
-
-/*
-
- SQL: MySQL, SQLite, PostgreeSQL, Oracle, Microsoft SQL server
- NOSQL: MongoDB, CouchDB, etc
-
-*/ 
